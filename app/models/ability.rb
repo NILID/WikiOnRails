b0df0f2093746bc9c1a14ffafdde6873780aad40
da <dm.ilin.aleksandrovich@gmail.com>
2020-04-25 21:58:44 +0300

    Change block to section

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,7 +7,7 @@ class Ability
     # Define abilities for the passed in user here. For example:
     #
     user ||= User.new # guest user (not logged in)
-    can :manage, [Page, Block]
+    can :manage, [Page, Section]
 
     if user.role? :admin
       can :manage, :all
